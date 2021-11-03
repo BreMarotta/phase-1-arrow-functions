86f9245fc8c6c9ea3d7a7e1f0d2b12952c17b9c8
Breanne <BreMarotta@gmail.com>
2021-11-03 11:15:48 -0400

    commit tree bec02668314ecda23e294180df931dfdfe0cb418

---
+++ index.js
@@ -0,0 +1,7 @@
+function divide(parameter1=2000, parameter2=100){
+    return parameter1/ parameter2;
+}
+
+const square = x => x*x;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
